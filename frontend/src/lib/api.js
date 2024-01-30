1a6f9278fed04200ee501d6881c6df0a2d944ecf
Rui Oliveira <rpoliveira02@gmail.com>
2024-01-30 16:39:13 +0000

    fix: add lib

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  responseType: "json",
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export async function getQuestion(id) {
+  const response = await API.get(`/question/${id}`);
+
+  return response.data;
+}
+
+export async function getQuestions() {
+  const response = await API.get("/questions");
+
+  return response.data;
+}
+
+export async function startQuestion() {
+  const response = await API.post("/buzz_start");
+
+  return response.data;
+}
+
+export async function getPlayers() {
+  const response = await API.get("/players");
+
+  return response.data;
+}
+
+export async function getWinners() {
+  const response = await API.get("/winners");
+
+  return response.data;
+}
+
+export async function setQuestion(id) {
+  const response = await API.post("/question", {
+    id: id,
+  });
+
+  return response.data;
+}
+
+export async function setPlayers(players) {
+  const response = await API.post("/players", {
+    players: players,
+  });
+
+  return response.data;
+}
+
+export async function answer(correct) {
+  const response = await API.post("/answer", {
+    correct: correct,
+  });
+
+  return response.data;
+}
