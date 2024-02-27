ac831e23ac9332578febea1af6118a1f74222802
Diogo Matos <diogomatosemail@gmail.com>
2024-02-27 11:47:41 +0000

    style: improvements to image positioning

--- frontend/src/components/GameState/GameAnsweringQuestion/index.tsx
+++ frontend/src/components/GameState/GameAnsweringQuestion/index.tsx
@@ -107,19 +107,18 @@ export default function GameAnsweringQuestion({
           {state.currentQuestion.value}
         </p>
       </div>
-      <div className="uppercase grow items-center flex flex-col font-extrabold text-6xl w-[80vw] text-center">
-        <p className="w-fit m-auto">
-          {state.currentQuestion.statement}
-        </p>{
+      <div className="uppercase grow items-center flex flex-col font-extrabold w-[80vw] text-center space-y-16">
+        {
           state.currentQuestion.image != "" && (
-            <Image
-              src={`/question_images/${state.currentQuestion.image}`}
+            <img
+              src={`/images/questions/${state.currentQuestion.image}`}
               alt="Question Image"
-              width={250}
-              height={250}
-              className="p-6"/>
+              className={`${role == "staff" ? 'h-[16rem]' : 'h-[26rem]'} rounded-xl`}/>
           )
         }
+        <p className={`w-fit m-auto ${state.currentQuestion.image != "" ? 'text-4xl' : 'text-6xl'}`}>
+          {state.currentQuestion.statement}
+        </p>
       </div>
       {role != "viewer" && (
         <div className="uppercase grow items-center flex font-extrabold text-2xl text-accent w-3/4 text-center">
